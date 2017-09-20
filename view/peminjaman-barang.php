2a7f0514f836d820e1d9a0420f374afdabc3dd0e
Gema Ulama Putra <cybermujahidz@gmail.com>
2017-09-20 12:41:57 +0700

    change floor to ceil in hours estimation

--- view/peminjaman-barang.php
+++ view/peminjaman-barang.php
@@ -133,7 +133,7 @@ require_once('_header.php');
                                              <td>$item[namaBarang]</td>
                                              <td>$item[namaPeminjam]</td>
                                              <td>".$item['waktuPinjam']."</td>
-                                             <td>".floor(abs(time()-strtotime($item['waktuPinjam']))/(60*60))." Jam</td>
+                                             <td>".ceil(abs(time()-strtotime($item['waktuPinjam']))/(60*60))." Jam</td>
                                              <td>
                                                  <a onclick='detailPeminjaman(".$item['idpeminjamanbarang'].")' class='btn btn-primary btn-xs'>Detail</a>
                                                  <a onclick='transaksiKembali(".$item['idpeminjamanbarang'].")' class='btn btn-success btn-xs'>Kembali</a>
@@ -196,7 +196,7 @@ require_once('_header.php');
                                       }
                                       $belumKembali=$db->fetch("select * from peminjamanbarang $find order by $sort limit ".(($pg-1)*40).",40");
                                       foreach($belumKembali as $item){
-                                        $durasi=(!empty($item['waktuKembali']))?floor(abs(strtotime($item['waktuKembali'])-strtotime($item['waktuPinjam']))/(60*60)):0;
+                                        $durasi=(!empty($item['waktuKembali']))?ceil(abs(strtotime($item['waktuKembali'])-strtotime($item['waktuPinjam']))/(60*60)):0;
                                         echo "
                                          <tr>
                                              <td>$item[namaBarang]</td>
